6254c5567b2f9ee3d80e900e8c0c99348c175af2
devmarxx <devmarxx@m-scene.de>
2021-02-21 23:29:58 +0100

    Corrected actual time processing

--- p8_weather.py
+++ p8_weather.py
@@ -42,9 +42,12 @@ import xml.etree.ElementTree as ET
 
 import os
 import datetime
+from datetime import timedelta
 
 weekDays = ("MON","TUE","WED","THU","FRI","SAT","SUN")
-hourNow = datetime.datetime.now().hour
+# It is not understood by now, why we need to substract 15 hours to get a 
+# "near actual time" temperature. How does this behave on different longitudes?
+hourNow = (datetime.datetime.now() - timedelta(hours=15)).hour
 
 # Put your python version here if needed (i.e. python3.9)
 my_env = ""
